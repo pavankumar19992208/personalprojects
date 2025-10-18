3763e80e46c13dc75780a183d18107d02bc74e2b
pavan jagarapu <pavan.jagarpu@potlamapp.com>
2025-10-18 23:01:23 +0530

    neuralife ai market added

--- frontend/neuralife-ai-market/vite.config.ts
+++ frontend/neuralife-ai-market/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vite.dev/confi/
 export default defineConfig({
   // Set the base to the final sub-path
   base: "/personalprojects/neuralife-ai-market/",
